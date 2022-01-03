47b3a8c238478f6bf3af80714803f4029fc09612
yungtristxn <85810951+yungtristxn@users.noreply.github.com>
2022-01-03 17:51:56 +0100

    description fix

--- cogs/albion_cog.py
+++ cogs/albion_cog.py
@@ -41,17 +41,15 @@ class AlbionCog(commands.Cog):
             
             embed = discord.Embed(title="Stats", description=f"**IGN:** {data['Name']}", color=0x1db9fc, timestamp=parser.parse(stats['Timestamp']))
             embed.add_field(name='Player Info', value=f"**Name:** {data['Name']}\n**Guild:** {data['GuildName']}\n**Alliance:** {data['AllianceName']}\n", inline=True)
-            embed.add_field(name='PvE Fame',value=f"**Total Pve Fame:** {pve['Total']:,}\n**Royal Zone:** {pve['Royal']:,}\n**Outlands:** {pve['Outlands']:,}\n**Avalon**: {pve['Avalon']:,}\n**Hellgate:** {pve['Hellgate']:,}\n**Corrupted Dungeon:** {pve['CorruptedDungeon']:,}", inline=True)
+            embed.add_field(name='PvE Fame',value=f"**Fame by killing mobs:** {pve['Total']:,}\n**Royal Zone:** {pve['Royal']:,}\n**Outlands:** {pve['Outlands']:,}\n**Avalon**: {pve['Avalon']:,}\n**Hellgate:** {pve['Hellgate']:,}\n**Corrupted Dungeon:** {pve['CorruptedDungeon']:,}", inline=True)
             embed.add_field(name='\u200b', value='\u200b', inline=True)
-            embed.add_field(name='Gathering',value=f"**Total Gathering Fame:** {gathering['All']['Total']:,}\n**Fiber Fame:** {gathering['Fiber']['Total']:,}\n**Hide Fame:** {gathering['Hide']['Total']}\n**Ore Fame:** {gathering['Ore']['Total']:,}\n**Wood Fame:** {gathering['Wood']['Total']:,}", inline=True)
+            embed.add_field(name='Gathering',value=f"**Total Gathering Fame:** {gathering['All']['Total']:,}\n**Fiber Fame:** {gathering['Fiber']['Total']:,}\n**Hide Fame:** {gathering['Hide']['Total']:,}\n**Ore Fame:** {gathering['Ore']['Total']:,}\n**Wood Fame:** {gathering['Wood']['Total']:,}", inline=True)
             embed.add_field(name='Other',value=f"**Total Crafting Fame:** {crafting['Total']:,}\n**Total Fishing Fame:** {stats['FishingFame']:,}\n**Total Farming Fame:** {stats['FarmingFame']:,}", inline=True)
             embed.add_field(name='\u200b', value='\u200b', inline=True)
             embed.set_footer(text='Data from:')
             await ctx.send(embed=embed)
         else: 
             await ctx.send('Player not found!')
-
-
 
 def setup(bot):
     bot.add_cog(AlbionCog(bot))
